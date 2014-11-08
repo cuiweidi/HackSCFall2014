411a2d6b9f0553491b933a2743c54173bf8fc132
Elizabeth Chu <Eli@guest-wireless-207-151-035-004.usc.edu>
2014-11-08 15:26:48 -0800

    after pull: adding 2 GUI's

---
+++ HackSC_Fooder/app/src/main/java/com/HackS/Fall2014/hacksc_fooder/RestaurantInfo.java
@@ -0,0 +1,15 @@
+package com.HackS.Fall2014.hacksc_fooder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Eli on 14/11/8.
+ */
+public class RestaurantInfo extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.restaurant_info);
+    }
+}
---
+++ HackSC_Fooder/app/src/main/java/com/HackS/Fall2014/hacksc_fooder/main.java
@@ -0,0 +1,16 @@
+package tutorial.com.hackschools.fooder_19;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.HackS.Fall2014.hacksc_fooder.R;
+
+public class main extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        }
+    }
+
