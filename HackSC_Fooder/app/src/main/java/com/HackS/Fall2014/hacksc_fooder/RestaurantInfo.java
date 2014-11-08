bd4e49a1980f4668d4a69f428615f46c1605d845
tianchez <tianchez@usc.edu>
2014-11-08 15:41:53 -0800

    From Frank 1

--- HackSC_Fooder/app/src/main/java/com/HackS/Fall2014/hacksc_fooder/RestaurantInfo.java
+++ HackSC_Fooder/app/src/main/java/com/HackS/Fall2014/hacksc_fooder/RestaurantInfo.java
@@ -10,6 +10,6 @@ public class RestaurantInfo extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.restaurant_info);
+        setContentView(R.layout.fragment_restaurant_info);
     }
 }
